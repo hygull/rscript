1f4ad8077c7d0822d06e57af9e0e5a3ba288f7a8
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-05-09 14:48:01 +0530

    40 toupper() and tolower() functions in R

---
+++ src/09_strings/toupper_tolower.R
@@ -0,0 +1,47 @@
+# {
+# 	"created_on" : "9 May 2017",
+# 	"aim_of_rscript" : "Using toupper() and tolower() in R",
+# 	"coded_by" : "Rishikesh Agrawani",
+# }
+
+#Example 1
+name <- "Steve Jobs"
+cat("Name : ", name,"\n")
+cat("Name(lower cased) : ", tolower(name),"\n")
+cat("Name(upper cased) : ", toupper(name), "\n")
+
+#Example 2
+details <- function(name, age, display="lower") {
+	cat("\nYour details :-\n")
+	if(display == "upper"){
+		cat("Name :- ", toupper(name), "\n")
+		cat("Age  :- ", toupper(age), "\n")
+	} else {
+		cat("Name :- ", tolower(name), "\n")
+		cat("Age  :- ", tolower(age), "\n")
+	}
+	
+}
+
+details("Michael Jackson", 50)	#Default display is upper
+details("Rishikesh Agrawani", 24,"upper")
+details(display="lower" ,name="Pentonic Firix", age=45)
+
+
+# Name :  Steve Jobs 
+# Name(lower cased) :  steve jobs 
+# Name(upper cased) :  STEVE JOBS 
+
+# Your details :-
+# Name :-  michael jackson 
+# Age  :-  50 
+
+# Your details :-
+# Name :-  RISHIKESH AGRAWANI 
+# Age  :-  24 
+
+# Your details :-
+# Name :-  pentonic firix 
+# Age  :-  45 
+
+
